b923ed9064b3d3506271a609a599c5bd238d626b
Eric Ayers <ericzundel@gmail.com>
2025-04-11 13:33:34 -0400

    Added code to reize the grid
    The grid can be resized.
    I'm having Flex layout woes!
    I am not able to get the pixels to be square
    and keep the same amount of width as before
    it starts.

--- etchasketch.js
+++ etchasketch.js
@@ -1,31 +1,57 @@
 console.log("Starting");
-containerElem = document.querySelector(".container");
-const NUM_ROWS = 16;
-const NUM_COLS = 16;
 
 let grid = [];
+let numRows = 16;
+let numCols = 16;
+const bodyElem = document.querySelector('body');
+const containerElem = document.querySelector('.container');
+const rowInputElem = document.querySelector('.rows');
+const columnInputElem = document.querySelector('.columns');
+const resetButtonElem = document.querySelector('.reset');
 
-// build the grid of divs
-for (let row = 0; row < NUM_ROWS; row++) {
-    let rowElem = document.createElement("div");
-    rowElem.className = 'row';
-    let rowElemArray = [];
-    for (let col = 0; col < NUM_COLS; col++) {
-        let cellElem = createCell(row, col);
-        rowElemArray.push(cellElem);
-        rowElem.appendChild(cellElem);
+rowInputElem.value = numRows;
+columnInputElem.value = numCols;
+
+resetButtonElem.addEventListener('click', (e) => {
+    numRows = Math.min(rowInputElem.value, 100);
+    numCols = Math.min(columnInputElem.value, 100);
+    buildGrid();
+    rowInputElem.value = numRows;
+    columnInputElem.value = numCols;
+});
+
+function buildGrid() {
+    // const width = window.innerWidth - bodyElem.style.paddingLeft - bodyElem.style.paddingRight;
+
+    while (containerElem.firstChild) {
+        containerElem.firstChild.remove()
     }
-    containerElem.appendChild(rowElem);
-    grid.push(rowElemArray);
-    
+
+    // build the grid of divs
+    for (let row = 0; row < numRows; row++) {
+        let rowElem = document.createElement("div");
+        rowElem.className = 'row';
+        let rowElemArray = [];
+        for (let col = 0; col < numCols; col++) {
+            let cellElem = createCell(row, col);
+            rowElemArray.push(cellElem);
+            rowElem.appendChild(cellElem);
+        }
+        // rowElem.style.flexBasis = String(100/numCols) + "%"
+        containerElem.appendChild(rowElem);
+        grid.push(rowElemArray);
+    }
 }
 
 function createCell(row, col) {
-    const cellElem =  document.createElement('div');
+    const cellElem = document.createElement('div');
     cellElem.classList.add("cell")
     cellElem.classList.add(`cell-${row}-${col}`);
     cellElem.addEventListener('mouseover', (e) => {
         cellElem.style.backgroundColor = "white";
     });
+    // cellElem.style.flexBasis = 100/numCols;
     return cellElem;
 }
+
+buildGrid();
